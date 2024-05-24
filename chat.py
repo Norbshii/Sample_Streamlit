5d8ce421e2d2568a07b3227692d90482315cf4cd
Norberto Pingoy [WVSU CS] <92943536+Norbshii@users.noreply.github.com>
2024-05-24 14:57:00 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -42,9 +42,9 @@ if st.button("Ask Gemini"):
             st.subheader("Conversation History:")
             for entry in st.session_state.chat_history:
                 if entry['type'] == "Question":
-                    st.markdown(f"<h3 style='font-weight:bold;'>You said:</h3>{entry['content']}", unsafe_allow_html=True)
+                    st.markdown(f"<h2 style='font-weight:bold;'>You said:</h2>{entry['content']}", unsafe_allow_html=True)
                 elif entry['type'] == "Response":
-                    st.markdown(f"<h3 style='font-weight:bold;'>Gemini replied:</h3>{entry['content']}", unsafe_allow_html=True)
+                    st.markdown(f"<h2 style='font-weight:bold;'>Gemini replied:</h2>{entry['content']}", unsafe_allow_html=True)
     else:
         st.warning("Please enter a question.")
 
