8bfd752b50497f45a399397793365615a84c8169
Norberto Pingoy [WVSU CS] <92943536+Norbshii@users.noreply.github.com>
2024-05-24 12:50:28 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -12,37 +12,32 @@ import textwrap
 
 import google.generativeai as genai
 
-from IPython.display import display
-from IPython.display import Markdown
-
-
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
+## Function to load OpenAI model and get responses
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 def get_gemini_response(question):
     
-    response =chat.send_message(question,stream=True)
+    response = chat.send_message(question, stream=True)
     return response
 
-##initialize our streamlit app
+## Initialize our streamlit app
 
 st.set_page_config(page_title="Q&A Demo")
 
 st.header("Gemini Application")
 
-input=st.text_input("Input: ",key="input")
+input = st.text_input("Input: ", key="input")
 
-
-submit=st.button("Ask the question")
+submit = st.button("Ask the question")
 
 ## If ask button is clicked
 
 if submit:
     
-    response=get_gemini_response(input)
+    response = get_gemini_response(input)
     st.subheader("The Response is")
     for chunk in response:
         print(st.write(chunk.text))
